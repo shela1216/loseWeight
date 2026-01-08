a0499180a6a283182c21227577ed1425a76d4c99
Shela Yang <shela1216@gmail.com>
2026-01-08 16:08:31 +0800

    優化餐點明細顯示

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC2xMAVAdN3674qtALhqblQncn33TwFPss",
+    authDomain: "loseweight-4b745.firebaseapp.com",
+    projectId: "loseweight-4b745",
+    storageBucket: "loseweight-4b745.firebasestorage.app",
+    messagingSenderId: "76431868318",
+    appId: "1:76431868318:web:98e0267325ea1fd9f8d68f",
+    measurementId: "G-V3ZXWD5HP7"
+};
